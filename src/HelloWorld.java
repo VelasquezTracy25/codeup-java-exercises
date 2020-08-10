5d6193e3f121761715acdf5482373f367d0c2fba
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-08-09 22:17:32 -0500

    completed HelloWorld and Main exercises

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld { //Class definition
+    public static void main(String[] args) { //Main Method
+        System.out.println("Hello, World!"); //System Output
+        System.out.println("Second output."); //System Output
+
+    }
+}
+
