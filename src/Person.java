d6d06356d1edd6a791c5de85027535701c11b373
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-08-13 17:10:03 -0500

    update

--- src/Person.java
+++ src/Person.java
@@ -1,32 +1,38 @@
-package util;
-
 public class Person{
 
-//Create util.Person class inside of src that has a private name property that is a string, and the following methods:
+//Create Person class inside of src that has a private name property that is a string, and the following methods:
+
+        private String name;
+
+//TODO: return the person's name
+        public String getName(){
+
+        return name;
+        }
+
+//TODO: change the name property to the passed value
+        public void setName(String name){
+
+        this.name = name;
+        }
+
+//TODO: print a message to the console using the person's name
+        public void sayHello(){
+            System.out.println("Hello" + name);
+        }
+
 
 //The class should have a constructor that accepts a `String` value and sets
 //the person's name to the passed string.
 //
-//Create a `main` method on the class that creates a new `util.Person` object and
+//Create a `main` method on the class that creates a new `Person` object and
 //tests the above methods.
-
-        private String name;
-
-        public String getName(){
-//TODO: return the person's name
-        MethodsExercises.Person.name = name;
-        }
-
-        public void setName(String name){
-//TODO: change the name property to the passed value
-        }
-        public void sayHello(){
-//TODO: print a message to the console using the person's name
-        }
 
 
     public static void main(String[] args) {
-
+    Person tracy = new Person();
+    tracy.name = "Tracy";
+        System.out.println(tracy);
     }
 
         }
