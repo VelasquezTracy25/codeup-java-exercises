bf652448939a6a664e21b48acbb561129ca8b834
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-08-16 02:00:16 -0500

    Fizzbuzz with if statements

--- src/Practice/FizzbuzzPractices.java
+++ src/Practice/FizzbuzzPractices.java
@@ -1,4 +1,22 @@
 package Practice;
 
 public class FizzbuzzPractices {
+    public static void main(String[] args) {
+
+        int randomNumber = (int) (Math.random() * 100);
+
+        if (randomNumber % 15 == 0)
+            System.out.println("The number is " + randomNumber + " and is divisible by 3 AND 5. FIZZBUZZ.");
+
+        else if (randomNumber % 3 == 0)
+            System.out.println("The number is " + randomNumber + " and is divisible by 3. Very Fizzy.");
+
+        else if (randomNumber % 5 == 0)
+            System.out.println("The number is " + randomNumber + " and is divisible by 5. Very Buzzy");
+
+        else
+            System.out.println("The number is " + randomNumber + " and is neither Fizzy nor Buzzy.");
+    }
+
 }
+
