70fbc6cfd1f91c542b94a4d40c377c6b24f4b5c8
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-08-16 02:20:57 -0500

    updated exercises. Complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
+
+    //call table of powers
+    tableOfPowers();
 /*
 While
 
@@ -12,58 +15,60 @@ Your output should look like this:
 
 5 6 7 8 9 10 11 12 13 14 15
  */
-//        int i = 5;
-//        while (i <= 15) {
-//            System.out.println(i);
-//            i++;
-//        }
-//
+
+        System.out.println("While number is greater than 5 and less than / equal to 15, print number.");
+        int j = 5;
+        while (j <= 15) {
+            System.out.println("Count is: " + j);
+            j++;
+        }
+
 
 /*
 A. Do While
 
 Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
-Alter your loop to count backwards by 5's from 100 to -10.
-Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
-
- 2
- 4
- 16
- 256
- 65536
 */
 
+        System.out.println("Start and 2 and count to 100 by 2.");
 
-// B. Do While
-//Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+        int a = 2;
+
+        do {
+            System.out.println("Count is: " + a);
+            a += 2;
+        }
+        while (a <= 100);
+
 //Alter your loop to count backwards by 5's from 100 to -10.
+
+        System.out.println("Start 100 and count to 0 by 5s.");
+
+        int b = 100;
+
+        do {
+            System.out.println("Count is: " + b);
+            b -= 5;
+        }
+        while (b >= 0);
+
+
 //Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
 
-//long k = 1000;
-//        do {
-//            System.out.println(k);
-//            k+=2;
-//            } while (k < -11);
-
-//long k = 100;
-//        do {
-//            System.out.println(k);
-//            k-=5;
-//            } while (k > -11);
-
-//long k = 2;
-//        do {
-//            System.out.println(k);
-//            k*=k;
-//            } while (k < 1000000);
-
+        System.out.println("Let's use a do while loop.");
+        long k = 2;
+        do {
+            System.out.println(k);
+            k *= k;
+        } while (k < 1000000);
 
 // C. For
 // Refactor the previous two exercises to use a for loop instead.
 
-//        for(long i = 2L; i < 1000000; i*=i) {
-//            System.out.println(i);
-//        }
+        System.out.println("Let's use a for loop.");
+        for (long i = 2L; i < 1000000; i *= i) {
+            System.out.println(i);
+        }
 
 // 2. Fizzbuzz
 //One of the most common interview questions for entry-level programmers is the FizzBuzz test. Developed by Imran Ghory, the test is designed to assess basic looping and conditional logic skills.
@@ -72,12 +77,27 @@ Create a do-while loop that starts at 2, and displays the number squared on each
 //        For the multiples of five: print “Buzz”.
 //        For numbers which are multiples of both three and five: print “FizzBuzz”.
 
-//        for (long i=1L; i < 100; i++){
-//            if (i % 15 == 0) System.out.println("FizzBuzz");
-//            else if (i % 3 == 0) System.out.println("Fizz");
-//            else if (i % 5 == 0) System.out.println("Buzz");
-//            else System.out.println(i);
-//        }
+        for (int i = 1; i < 100; i++) {
+            if (i % 15 == 0) System.out.println("FizzBuzz");
+            else if (i % 3 == 0) System.out.println("Fizz");
+            else if (i % 5 == 0) System.out.println("Buzz");
+            else System.out.println(i);
+        }
+
+        randomNumber();
+    }
+
+    public static void randomNumber() {
+        int randomNumber = (int) (Math.random() * 100);
+        if (randomNumber % 15 == 0)
+            System.out.println("The random number is " + randomNumber + " and is divisible by 3 AND 5. FIZZBUZZ.");
+        else if (randomNumber % 3 == 0)
+            System.out.println("The random number is " + randomNumber + " and is divisible by 3. Very Fizzy.");
+        else if (randomNumber % 5 == 0)
+            System.out.println("The random number is " + randomNumber + " and is divisible by 5. Very Buzzy");
+        else
+            System.out.println("The random number is " + randomNumber + " and is neither Fizzy nor Buzzy.");
+    }
 
 //3. Display a table of powers.
 //        Prompt the user to enter an integer.
@@ -87,6 +107,9 @@ Create a do-while loop that starts at 2, and displays the number squared on each
 //        Only continue if the user agrees to.
 //        Example Output
 
+public static void tableOfPowers(){
+
+    {
         Scanner scanner = new Scanner(System.in);
         String userResponse;
         do {
@@ -104,14 +127,8 @@ Create a do-while loop that starts at 2, and displays the number squared on each
             userResponse = scanner.next();
         } while (userResponse.equals("y"));
 
-
-//3. Display a table of powers.
-
-
-
-
-
-
     }
+
+}
 }
 
