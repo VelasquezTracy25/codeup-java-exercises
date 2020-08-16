2673ce62d0c7334f4055f9035b85327a4cb8f68e
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-08-16 02:37:20 -0500

    complete

--- src/Practice/FizzbuzzPractices.java
+++ src/Practice/FizzbuzzPractices.java
@@ -7,16 +7,14 @@ public class FizzbuzzPractices {
 
         if (randomNumber % 15 == 0)
             System.out.println("The number is " + randomNumber + " and is divisible by 3 AND 5. FIZZBUZZ.");
-
         else if (randomNumber % 3 == 0)
             System.out.println("The number is " + randomNumber + " and is divisible by 3. Very Fizzy.");
-
         else if (randomNumber % 5 == 0)
             System.out.println("The number is " + randomNumber + " and is divisible by 5. Very Buzzy");
-
         else
             System.out.println("The number is " + randomNumber + " and is neither Fizzy nor Buzzy.");
     }
 
 }
+
 
