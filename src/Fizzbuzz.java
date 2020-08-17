b872b5f9dfafe0f36e9c6f17241df59393d0caa8
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-08-17 12:09:18 -0500

    8/17 warmup

--- src/Fizzbuzz.java
+++ src/Fizzbuzz.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Fizzbuzz {
-}
+    public static void main(String[] args) {
+//Create a class named FizzBuzz, and write a fizzBuzz method that will loop to the number passed and complete the FizzBuzz problem.
+        fizzBuzz(50);
+
+    }
+
+    public static void fizzBuzz(int num) {
+        for (var i = 1; i <= num; i++) {
+            if (i % 15 == 0)
+                System.out.println("The number is " + i + " and is divisible by 3 AND 5. FIZZBUZZ.");
+            else if (i % 3 == 0)
+                System.out.println("The number is " + i + " and is divisible by 3. Very Fizzy.");
+            else if (i % 5 == 0)
+                System.out.println("The number is " + i + " and is divisible by 5. Very Buzzy");
+            else
+                System.out.println("The number is " + i + " and is neither Fizzy nor Buzzy.");
+        }
+    }
+    }
