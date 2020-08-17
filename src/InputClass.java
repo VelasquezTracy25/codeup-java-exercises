34e14fa3f75c3d23d7287eb4793e2c0fde7d63a5
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-08-17 12:13:54 -0500

    cleaned up

--- src/InputClass.java
+++ src/InputClass.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-//import InputMismatchExpection;
+
 
 public class InputClass {
 public static void main(String[] args){
 
 Scanner scanner = new Scanner(System.in);
-//
-//    System.out.print("\n\nEnter Something: ");
-//    String userInput= scanner.next();
-//    System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+    System.out.print("\n\nEnter Something: ");
+    String userInput= scanner.next();
+    System.out.println("You entered: --> \"" + userInput + "\" <--");
 
 
 
