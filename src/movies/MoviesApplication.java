898da8191102622ccbb0e181da1338bf917e1b03
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-08-17 12:13:12 -0500

    completed movies project

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -8,6 +8,7 @@ public class MoviesApplication {
     }
 
     public static void runApp() {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("What would you like to do?\n" +
                 "        0 - exit\n" +
@@ -15,17 +16,21 @@ public class MoviesApplication {
                 "        2 - view movies in the animated category\n" +
                 "        3 - view movies in the drama category\n" +
                 "        4 - view movies in the horror category\n" +
-                "        5 - view movies in the scifi category");
+                "        5 - view movies in the scifi category\n" +
+                "\n" + "Select a number option.");
         int userPick = scanner.nextInt();
+
 
         if (userPick == 0) {
             System.out.println("Exiting...");
         } else if (userPick == 1) {
-            System.out.print("Displaying all movies: \n");
-            Movie[] movies = MoviesArray.findAll();
+            System.out.println("\n\nDisplaying all movies: \n");
+            for (Movie movie : MoviesArray.findAll()) {
+            System.out.printf("%s - %s\n", movie.getName(), movie.getCategory());
+            }
             rerun();
         } else if (userPick == 2) {
-            System.out.println("Displaying all animated movies: \n");
+            System.out.println("\n\nDisplaying all animated movies: \n");
             categorizeMovies("animated");
             rerun();
         } else if (userPick == 3) {
@@ -40,6 +45,9 @@ public class MoviesApplication {
             System.out.println("Displaying all scifi movies: \n");
             categorizeMovies("scifi");
             rerun();
+        } else {
+            System.out.println("\nEnter a valid number.\n\n");
+            runApp();
         }
     }
 
@@ -47,15 +55,14 @@ public class MoviesApplication {
         for (Movie movie : MoviesArray.findAll()) {
             if (movie.getCategory().equalsIgnoreCase(category)) {
                 //Display the movie.
-                System.out.printf("%s -- %s\n", movie.getName(), movie.getCategory());
+                System.out.printf("%s - %s\n", movie.getName(), movie.getCategory());
             }
         }
     }
 
-
     public static void rerun() {
         Scanner scannerific = new Scanner(System.in);
-        System.out.println("\nSearch again?");
+        System.out.println("\n" + "Search again? (y/n)");
         String yesNo = scannerific.nextLine().toLowerCase();
         if ((yesNo.equals("yes")) || (yesNo.equals("y"))) {
             runApp();
