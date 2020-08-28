008e18ab1875b01f1b495b6715891c441677c36c
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-08-28 09:06:08 -0500

    updates to Input test

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -3,17 +3,15 @@ package util;
 public class InputTest {
 //Create another class named InputTest that has a static main method that uses all the methods from the Input class.
 
-
     public static void main(String[] args) {
         Input input = new Input();
-        do {
-            System.out.print("Enter a whole integer number: ");
-            String userInput = input.getString();
-//            input.getInt(userInput);
-        } while( input.yesNo() );
-        do {
-            input.getDouble(1, 10);
-        } while( input.yesNo() );
+
+        System.out.println("input.getInt(1,5) = " + input.getInt(1,5));
+
+        System.out.println("input.getInt() = " + input.getInt());
+
+        System.out.println("input.getDouble() = " + input.getDouble());
+        System.out.println("input.getDouble(min, max) = " + input.getDouble(5.5, 10.90));
     }
 }
 
